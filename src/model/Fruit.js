16b4d5d988767b221c0607cf6b02b1445fce22a8
rasool-js <rasool3499@gmail.com>
2022-07-08 12:24:07 +0430

    chor Order create

--- src/model/Fruit.js
+++ src/model/Fruit.js
@@ -1,10 +1,8 @@
-const { link, db } = require('./connect.js');
+const { link, db } = require("./connect.js");
 
 class Fruit {
-	constructor() {}
-
-	static QUERY = {
-		CREATE: `
+  static QUERY = {
+    CREATE: `
 		CREATE TABLE IF NOT EXISTS Fruit (
 		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 		name TEXT,
@@ -12,99 +10,153 @@ class Fruit {
 		quantity REAL DEFAULT 0,
 		image TEXT
 	)`,
-		INSERT: `INSERT INTO Fruit (name, cost, quantity, image) VALUES (?, ?, ?, ?)`,
-		UPDATE: {
-			ALL: `UPDATE Fruit SET name = ?, cost = ?, quantity = ?, image = ? WHERE id = ?`,
-			NAME: `UPDATE Fruit SET name = ? WHERE id = ?`,
-			COST: `UPDATE Fruit SET cost = ? WHERE id = ?`,
-			QUANTITY: `UPDATE Fruit SET quantity = ? WHERE id = ?`,
-			IMAGE: `UPDATE Fruit SET image = ? WHERE id = ?`,
-		},
-		SELECT: {
-			ALL: `SELECT * FROM Fruit`,
-			BY_ID: `SELECT * FROM Fruit WHERE id = ?`,
-		},
-		DELETE: `DELETE FROM Fruit WHERE id = ?`,
-		DROP: `DROP TABLE IF EXISTS Fruit`,
-	};
+    INSERT: `INSERT INTO Fruit (name, cost, quantity, image) VALUES (?, ?, ?, ?)`,
+    UPDATE: {
+      ALL: `UPDATE Fruit SET name = ?, cost = ?, quantity = ?, image = ? WHERE id = ?`,
+      NAME: `UPDATE Fruit SET name = ? WHERE id = ?`,
+      COST: `UPDATE Fruit SET cost = ? WHERE id = ?`,
+      QUANTITY: `UPDATE Fruit SET quantity = ? WHERE id = ?`,
+      IMAGE: `UPDATE Fruit SET image = ? WHERE id = ?`,
+    },
+    SELECT: {
+      ALL: `SELECT * FROM Fruit`,
+      BY_ID: `SELECT * FROM Fruit WHERE id = ?`,
+    },
+    DELETE: `DELETE FROM Fruit WHERE id = ?`,
+    DROP: `DROP TABLE IF EXISTS Fruit`,
+  };
 
-	static init() {
-		return db.run(Fruit.QUERY.CREATE);
-	}
+  static init() {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.CREATE, function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve();
+      });
+    });
+  }
 
-	static readAll() {
-		return db.all(Fruit.QUERY.SELECT.ALL);
-	}
+  static readAll() {
+    return db.all(Fruit.QUERY.SELECT.ALL, function (error, rows) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      return resolve(rows);
+    });
+  }
 
-	static readById(id) {
-		return db.all(Fruit.QUERY.SELECT.BY_ID);
-	}
+  static readById(id) {
+    return db.all(Fruit.QUERY.SELECT.BY_ID, id, function (error, rows) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      return resolve(rows);
+    });
+  }
 
-	static insert(data) {
-		return db.run(Fruit.QUERY.INSERT, data, function (error) {
-			return this.lastId;
-		});
-	}
+  static insert(data) {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.INSERT, data, function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve(this.lastId);
+      });
+    });
+  }
 
-	static updateAll(id, newData) {
-		return db.run(
-			Fruit.QUERY.UPDATE.ALL,
-			[...newData, id],
-			function (error) {
-				return error;
-			},
-		);
-	}
+  static updateAll(id, newData) {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.UPDATE.ALL, [...newData, id], function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve(this.changes);
+      });
+    });
+  }
 
-	static updateName(id, newName) {
-		return db.run(Fruit.QUERY.UPDATE.NAME, [newName, id], function (error) {
-			return error;
-		});
-	}
+  static updateName(id, newName) {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.UPDATE.NAME, [newName, id], function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve(this.changes);
+      });
+    });
+  }
 
-	static updateCost(id, newCost) {
-		return db.run(Fruit.QUERY.UPDATE.COST, [newCost, id], function (error) {
-			return error;
-		});
-	}
+  static updateCost(id, newCost) {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.UPDATE.COST, [newCost, id], function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve(this.changes);
+      });
+    });
+  }
 
-	static updateQuantity(id, newQuantity) {
-		return db.run(
-			Fruit.QUERY.UPDATE.QUANTITY,
-			[newQuantity, id],
-			function (error) {
-				return error;
-			},
-		);
-	}
+  static updateQuantity(id, newQuantity) {
+    return new Promise(function (resolve, reject) {
+      return db.run(
+        Fruit.QUERY.UPDATE.QUANTITY,
+        [newQuantity, id],
+        function (error) {
+          if (error) {
+            reject(error);
+            return;
+          }
+          return resolve(this.changes);
+        }
+      );
+    });
+  }
 
-	static updateImage(id, newImage) {
-		return db.run(
-			Fruit.QUERY.UPDATE.IMAGE,
-			[newImage, id],
-			function (error) {
-				return error;
-			},
-		);
-	}
+  static updateImage(id, newImage) {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.UPDATE.IMAGE, [newImage, id], function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve(this.changes);
+      });
+    });
+  }
 
-	remove(id) {
-		return new Promise(function (resolve, reject) {
-			return db.run(Fruit.QUERY.DELETE, id, function (error) {
-				if (error) {
-					reject(error);
-					return;
-				}
-				return resolve(this.changes);
-			});
-		});
-	}
+  remove(id) {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.DELETE, id, function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve(this.changes);
+      });
+    });
+  }
 
-	static drop() {
-		return db.run(Fruit.QUERY.DROP, function (error) {
-			return error;
-		});
-	}
+  static drop() {
+    return new Promise(function (resolve, reject) {
+      return db.run(Fruit.QUERY.DROP, function (error) {
+        if (error) {
+          reject(error);
+          return;
+        }
+        return resolve();
+      });
+    });
+  }
 }
 
 module.exports = Fruit;
